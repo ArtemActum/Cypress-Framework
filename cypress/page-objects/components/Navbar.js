33bf108350d056e01e9ec14f57ea3e087ecab812
Artem <artem.mindsadyrov@actum.digital.com>
2022-06-15 16:22:05 +0200

    15.06

--- cypress/page-objects/components/Navbar.js
+++ cypress/page-objects/components/Navbar.js
@@ -1,26 +1,26 @@
 export default class Navbar {
-    static clickOnLogo() {
-        cy.get('.brand').click()
-    }
+	static clickOnLogo() {
+		cy.get(".brand").click()
+	}
 
-    static search(text) {
-        cy.get('.#searchTerm').type(`${text} {enter}`)
-    }
+	static search(text) {
+		cy.get(".#searchTerm").type(`${text} {enter}`)
+	}
 
-    static displaySignInButton() {
-        cy.isVisible('#signin_button')
-    }
+	static displaySignInButton() {
+		cy.isVisible("#signin_button")
+	}
 
-    static clickSignIn() {
-        cy.get('#signin_button').click()
-    }
+	static clickSignIn() {
+		cy.get("#signin_button").click()
+	}
 
-    static clickSettings() {
-        cy.contains('Settings').click()
-    }
+	static clickSettings() {
+		cy.contains("Settings").click()
+	}
 
-    static logout() {
-        cy.contains('username').click()
-        cy.contains('#logout_link').click()
-    }
+	static logout() {
+		cy.contains("username").click()
+		cy.get("#logout_link").click()
+	}
 }
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,10 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
-
 module.exports = (on, config) => {
-  addMatchImageSnapshotPlugin(on, config)
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
 }
